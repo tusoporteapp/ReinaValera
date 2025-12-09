15811ec9c913ab164e2ef646afb4b89f87c3e922
TuSoporteApp <tusoporte.app@gmail.com>
2025-12-09 13:47:45 -0500

    gfgjfgj

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker para caché offline de la Biblia Libre
-const CACHE_NAME = 'biblia-vbl-v7.0.0';
+const CACHE_NAME = 'biblia-vbl-v7.0.1';
 const BIBLE_DATA_CACHE = 'biblia-data-v2';
 
 const STATIC_ASSETS = [
